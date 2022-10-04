783b896bf185e1941d1fe265899bd2c6da9ebc71
Lee In Seok <tjrkd222@gmail.com>
2022-10-05 02:22:06 +0900

    1644 solve - two pointer

--- TwoPointer/1644.G3.js
+++ TwoPointer/1644.G3.js
@@ -5,4 +5,30 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const nums = input[0].split(" ");
 
-console.log(nums);
+const num = parseInt(nums[0]);
+
+let primes = [];
+
+let check = new Array(num + 1).fill(true);
+for (let i = 2; i * i <= num; i++) {
+  if (!check[i]) continue;
+  for (let j = i * i; j <= num; j += i) {
+    check[j] = false;
+  }
+}
+for (let i = 2; i <= num; i++) {
+  if (check[i]) primes.push(i);
+}
+
+let tmp = 0;
+let start = 0, end = 0;
+let result = 0;
+
+while(end < primes.length) {
+    tmp += primes[end++];
+    while(tmp > num) tmp -= primes[start++];
+
+    if (tmp === num) result ++;
+}
+
+console.log(result);
